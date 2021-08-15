b6453105a80b03387b23ccfd304a0adcf43a3331
xuejingfei <xue.jingfei@immomo.com>
2021-08-15 19:45:22 +0800

    二叉树中的最大路径和

---
+++ src/tree/MaxPathSum.java
@@ -0,0 +1,26 @@
+package tree;
+
+public class MaxPathSum {
+    int maxPathSum = 0;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) return 0;
+        maxGain(root);
+        return maxPathSum;
+    }
+
+
+    /**
+     * 最大贡献值
+     * @param root
+     * @return
+     */
+    public int maxGain(TreeNode root) {
+        if (root == null) return 0;
+        int leftGain = Math.max(0,maxGain(root.left));
+        int rightGain = Math.max(0,maxGain(root.right));
+        int max = leftGain + rightGain + root.val;
+        maxPathSum = Math.max(maxPathSum,max);
+        return root.val + Math.max(leftGain,rightGain);
+    }
+}
