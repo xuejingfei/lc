1009676046d28f8ad0b64611f603717f971afba1
xuejingfei <xue.jingfei@immomo.com>
2021-10-15 14:38:59 +0800

    打印二叉树右侧的节点

---
+++ src/tree/RightSideView.java
@@ -0,0 +1,35 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RightSideView {
+
+
+    /**
+     * 从顶部到底部的顺序，右侧看到的节点
+     * @param root
+     * @return
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNode lastNode = null;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i =0;i<size;i++) {
+                TreeNode top = queue.poll();
+                if (i == size-1) {
+                    lastNode = top;
+                }
+                if (top.left!=null) queue.add(top.left);
+                if (top.right!=null) queue.add(top.right);
+            }
+            result.add(lastNode.val);
+        }
+        return result;
+    }
+}
---
+++ src/tree/TreeNode.java
@@ -0,0 +1,8 @@
+package tree;
+
+public class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+    public int val;
+
+}
