2abf2789943df7c9085b18be37e90d313679dd7f
xuejingfei <xuejingfei001@gmail.com>
2021-10-20 23:29:33 +0800

    合并两个有序链表

---
+++ src/list/MergeTwoLists.java
@@ -0,0 +1,28 @@
+package list;
+
+public class MergeTwoLists {
+
+    /**
+     * 合并两个升序链表
+     * @param l1
+     * @param l2
+     */
+    private ListNode mergeTwoLists(ListNode l1,ListNode l2) {
+        ListNode dummp = new ListNode(-1);
+        ListNode tail = dummp;
+        while (l1 !=null && l2!=null){
+            if (l1.val<l2.val) {
+                tail.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                tail.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if (l1 == null) tail.next = l2;
+        if (l2 == null) tail.next = l1;
+
+        return dummp.next;
+    }
+}
