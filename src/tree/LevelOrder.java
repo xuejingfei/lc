d8c9302e96c4da41a571889ae26cc46ee5d9a613
xuejingfei <xuejingfei001@gmail.com>
2021-11-08 23:14:00 +0800

    54:层次遍历

---
+++ src/tree/LevelOrder.java
@@ -0,0 +1,31 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LevelOrder {
+
+    /**
+     * 层次遍历
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        LinkedList<List<Integer>> ans = new LinkedList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        if (root!=null) queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for(int i=0;i<size;i++) {
+                TreeNode top = queue.poll();
+                level.add(top.val);
+                if (top.left!=null) queue.add(top.left);
+                if (top.right!=null) queue.add(top.right);
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
+}
