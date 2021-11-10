0ef0d26737ef9c426fe092703226803c115592c1
xuejingfei <xuejingfei001@gmail.com>
2021-11-10 22:11:37 +0800

    58:最大数补全快速排列

--- src/array/LargestNumber.java
+++ src/array/LargestNumber.java
@@ -36,6 +36,50 @@ public class LargestNumber {
     }
 
 
+    public String largestNumber1(int[] nums) {
+        quickSort(nums,0,nums.length-1);
+        StringBuilder ans = new StringBuilder();
+        for (int i:nums) {
+            ans.append(i);
+        }
+        if (ans.charAt(0) == '0') return "0";
+        return ans.toString();
+    }
+
+
+    public void quickSort(int[] nums,int low,int high) {
+        if (low>high) return;
+        int i = low;
+        int j = high;
+        int lowV = nums[low];
+        while (i<j) {
+            while (compare(nums[j],lowV)&& i<j) {
+                j--;
+            }
+
+            while (!compare(nums[i],lowV)&& i<j) {
+                i++;
+            }
+            if (i<j) {
+                int temple = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temple;
+            }
+        }
+        nums[low] = nums[i];
+        nums[i] = lowV;
+        quickSort(nums,low,i-1);
+        quickSort(nums,i+1,high);
+    }
+
+
+    public boolean compare(int a,int b) {
+        String aValue = a+""+b;
+        String bValue = b+"" +a;
+        return aValue.compareTo(bValue)>=0;
+    }
+
+
 
 
 }
