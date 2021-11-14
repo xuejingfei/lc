789a527079370fd2f2077f3f107fdb33528b3b40
xuejingfei <xuejingfei001@gmail.com>
2021-11-15 00:14:59 +0800

    66:股票价格跨度

---
+++ src/singleInvokeStack/StockSpanner.java
@@ -0,0 +1,23 @@
+package singleInvokeStack;
+
+import java.util.Stack;
+
+public class StockSpanner {
+    private Stack<Integer> prices,weights;
+
+    public StockSpanner() {
+        prices = new Stack<>();
+        weights = new Stack<>();
+    }
+
+    public int next(int price) {
+        int w =1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            w+=weights.pop();
+        }
+        prices.push(price);
+        weights.push(w);
+        return w;
+    }
+}
